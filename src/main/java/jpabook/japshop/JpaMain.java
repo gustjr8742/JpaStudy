1c80910b3ef56c398aa6b4e67428bcd299609df0
HyunSeok <gustjr8742@naver.com>
2021-05-18 13:58:29 +0900

    연관관계 매핑 시작

--- src/main/java/jpabook/japshop/JpaMain.java
+++ src/main/java/jpabook/japshop/JpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.japshop;
 
+import jpabook.japshop.domain.Order;
+import jpabook.japshop.domain.OrderItem;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -15,6 +18,20 @@ public class JpaMain {
         tx.begin();
 
         try {
+//            Order order = new Order();
+//            order.addOrderItem(new OrderItem()); // 양방향 매핑을 사용할 때 사용 예시
+
+
+            //양방향 매핑 안해도 되는것을 보여주는 코드
+            Order order = new Order();
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+
+            em.persist(orderItem);
+
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/jpabook/japshop/domain/Member.java
+++ src/main/java/jpabook/japshop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.japshop.domain;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -12,6 +14,13 @@ public class Member {
     private String city;
     private String street;
     private String zipcode;
+
+
+    //실질적으로는 쓰이는 일이 잘 없으나, 예제이므로 작성
+    //실무적으로 잘 사용하지 않음(잘못된 코드일 확률이 있음)
+    @OneToMany
+    @JoinColumn(name = "MEMBER_ID")
+    private List<Order> orders = new ArrayList<>();
 
     public Long getId() {
         return id;
